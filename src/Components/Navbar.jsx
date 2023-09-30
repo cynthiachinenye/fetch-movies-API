d1edb2ae86db1b2c02a423dff017a090c613ab89
cynthiachinenye <cynthiachiagu@gmail.com>
2023-09-30 13:47:04 +0100

    new

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,25 +1,29 @@
-import React,{Fragment} from 'react'
-import {HiSearch} from 'react-icons/hi'
+import React, {  useState } from 'react'
+import { HiSearch } from 'react-icons/hi'
+import '../Styles/navbar.scss'
 
 function Navbar() {
-  return (
-    <Fragment>
-    <nav className='nav'>
-      <div className='nav-options'>
-      <h1>LOGO..</h1>
-      <span>Movies</span>
-      <span>Tv Shows</span>
-      <span>Trending</span>
-      <span>Pricing</span>
-      </div>
-      <input type='text' placeholder='Search for Movies'/>
-      <HiSearch/>
-      <div id='color-switch'>
-      <div id='color-switcher-move'></div>
-      </div>
-    </nav>
- </Fragment>
-  )
+    const [toggle, setToggle] = useState([])
+    return (
+        <div className='navbar'>
+            <nav className='nav'>
+                <div className='nav-options'>
+                    <h1>LOGO..</h1>
+                    <span>Movies</span>
+                    <span>Tv Shows</span>
+                    <span>Trending</span>
+                    <span>Pricing</span>
+                </div>
+                <div className='search-input'>
+                <input type='text' placeholder='Search for Movies' />
+                <HiSearch className='icon' />
+                </div>
+                <div id='color-switch' onClick={()=> setToggle(!toggle)}>
+                    <div id={ toggle? 'color-switch' : 'color-switcher-move'}></div>
+                </div>
+            </nav>
+        </div>
+    )
 }
 
 export default Navbar
