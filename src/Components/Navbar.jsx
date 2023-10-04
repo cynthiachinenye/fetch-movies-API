43e8a0a8ef549f5aa630dcd374be543b6b335b16
cynthiachinenye <cynthiachiagu@gmail.com>
2023-10-04 17:32:59 +0100

    update

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, {  useState } from 'react'
+import React, { useState } from 'react'
 import { HiSearch } from 'react-icons/hi'
 import '../Styles/navbar.scss'
 
@@ -7,19 +7,19 @@ function Navbar() {
     return (
         <div className='navbar'>
             <nav className='nav'>
-                <div className='nav-options'>
-                    <h1>LOGO..</h1>
-                    <span>Movies</span>
-                    <span>Tv Shows</span>
-                    <span>Trending</span>
-                    <span>Pricing</span>
+                <div className='nav-opt hide '>
+                    <h1 id={toggle ? "" : 'heading'}>LOGO..</h1>
+                    <span id={toggle ? "movies" : "movie-light"}>Movies</span>
+                    <span id={toggle ? "movies" : "tv-light"}>Tv Shows</span>
+                    <span id={toggle ? "movies" : "trend-light"}>Trending</span>
+                    <span id={toggle ? "movies" : "price-light"}>Pricing</span>
                 </div>
                 <div className='search-input'>
-                <input type='text' placeholder='Search for Movies' />
-                <HiSearch className='icon' />
+                    <input type='text' placeholder='Search for Movies' />
+                    <HiSearch className='icon' />
                 </div>
-                <div id='color-switch' onClick={()=> setToggle(!toggle)}>
-                    <div id={ toggle? 'color-switch' : 'color-switcher-move'}></div>
+                <div id='color-switch' onClick={() => setToggle(!toggle)}>
+                    <div id={toggle ? 'color-switcher-move' : 'color-switcher-moved'}></div>
                 </div>
             </nav>
         </div>
