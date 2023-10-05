f245ca4a3124dbc454846114e7673e333bb3c3d7
cynthiachinenye <cynthiachiagu@gmail.com>
2023-10-05 10:59:29 +0100

    update

--- src/Components/Movies.jsx
+++ src/Components/Movies.jsx
@@ -1,17 +1,43 @@
 import axios from 'axios'
-import React from 'react'
+import React, { useEffect,useState } from 'react'
+import { Fragment } from 'react'
+import {AiFillPlayCircle} from 'react-icons/ai'
+import noImage from '../Assets/javier-esteban-8At6XBgVyyY-unsplash.jpg'
 
 function Movies() {
+  const [movieData, setMovieData] =useState([])
+  const image = 'https://image.tmdb.org/t/p/w500/'
+
   async function movieShow (){
     const data = await axios.get("https://api.themoviedb.org/3/discover/movie?api_key=79d50d0b258ce81609b056767b5f5166",
-    {
-
-    })
+    {params:{
+      api_key: '79d50d0b258ce81609b056767b5f5166'
+    }
+      
+  })
+    const results = data.data.results
+    setMovieData(results)
   }
+ useEffect(()=>{
+  movieShow();
+ },[])
+ console.log(movieData)
   return (
-    <div>
-      <h1>movies </h1>
-    </div>
+    <Fragment>
+      {
+        movieData.map((movies)=> {
+          return(
+            <Fragment>
+          <div id='container'>
+           <AiFillPlayCircle color='green' fontSize={40} id='play-icon'/>
+           <img src= { movies.poster_path ?`${image}${movies.poster_path}`: noImage} alt=''/>
+           <h3>{movies.title}</h3>
+          </div>
+          </Fragment>
+          )
+        })
+      }
+    </Fragment>
   )
 }
 
